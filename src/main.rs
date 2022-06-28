d71981085a7b88d928f5b996336c0f5289e6fd6a
raman1383 <raman83shariati@gmail.com>
2022-06-28 23:34:17 +0430

    r

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,4 @@ pub mod sorting_and_searching_algs;
 pub mod tree_based_dsa_1;
 pub mod tree_based_dsa_2;
 
-fn main() {
-    // swap without temp
-
-    let mut x = 10;
-    let mut y = 5;
-
-    x = x + y;
-    y = x - y;
-    x = x - y;
-
-    println!("x = {x}, y = {y}");
-
-    println!("{}", other_algs::fun::find_factorial_2(5));
-    println!("{}", other_algs::fun::sum_of_first_10_pow_11_naturals());
-
-    let mut stack_1 = data_structures_1::stack::Stack::with_capacity(10);
-
-    stack_1.push(1);
-    stack_1.push(2);
-    stack_1.push(3);
-    stack_1.print_out();
-    stack_1.pop();
-    stack_1.print_out();
-}
+fn main() {}
