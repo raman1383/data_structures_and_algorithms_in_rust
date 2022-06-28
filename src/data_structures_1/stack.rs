9fb3411ec0a21f0c5e80389a07c8882fe67a615c
raman1383 <raman83shariati@gmail.com>
2022-06-28 10:05:24 +0430

    r

--- src/data_structures_1/stack.rs
+++ src/data_structures_1/stack.rs
@@ -1,29 +1,36 @@
 /// A linear DS following LIFO
 pub struct Stack {
-    top_ptr: Option<i128>,
     count: i128,
-    // elements
+    top: i128,
+    items: Vec<i128>,
 }
 
 impl Stack {
     pub fn new() -> Stack {
         Stack {
-            top_ptr: None,
-            count: -1,
+            items: Vec::with_capacity(10),
+            count: 0,
+            top: -1,
         }
     }
 
-    pub fn push(&mut self) {
+    pub fn push(&mut self, item_to_push: i128) {
         if !self.is_full() {
+            self.top = self.top + 1;
+            self.items[self.top as usize] = item_to_push;
             self.count = self.count + 1;
-            self.top_ptr = Some(self.top_ptr.unwrap() + 1)
+        } else {
+            println!("!!! FULL !!!")
         }
     }
 
-    pub fn pop(&mut self) {
+    pub fn pop(&mut self, item_to_pop: i128) {
         if !self.is_empty() {
+            self.top = self.top - 1;
+            self.items[self.top as usize] = item_to_pop;
             self.count = self.count - 1;
-            self.top_ptr = Some(self.top_ptr.unwrap() - 1)
+        } else {
+            println!("!!! Empty !!!")
         }
     }
 
@@ -43,5 +50,11 @@ impl Stack {
         }
     }
 
-    pub fn peek(&self) {}
+    // pub fn peek(&self) {}
+
+    pub fn print_stack(&self) {
+        for i in 0..self.count {
+            println!("{}", self.items[i as usize])
+        }
+    }
 }
