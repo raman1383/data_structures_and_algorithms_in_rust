aba2073d74a17f381c65be8ac55f35ba786d2753
raman1383 <raman83shariati@gmail.com>
2022-06-28 09:26:31 +0430

    r

--- src/data_structures_1/stack.rs
+++ src/data_structures_1/stack.rs
@@ -1,8 +1,47 @@
 /// A linear DS following LIFO
-pub struct Stack {}
+pub struct Stack {
+    top_ptr: Option<i128>,
+    count: i128,
+    // elements
+}
 
 impl Stack {
-    pub fn push() {}
+    pub fn new() -> Stack {
+        Stack {
+            top_ptr: None,
+            count: -1,
+        }
+    }
 
-    pub fn pop() {}
+    pub fn push(&mut self) {
+        if !self.is_full() {
+            self.count = self.count + 1;
+            self.top_ptr = Some(self.top_ptr.unwrap() + 1)
+        }
+    }
+
+    pub fn pop(&mut self) {
+        if !self.is_empty() {
+            self.count = self.count - 1;
+            self.top_ptr = Some(self.top_ptr.unwrap() - 1)
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        if self.count == -1 {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_full(&self) -> bool {
+        if self.count == -1 {
+            false
+        } else {
+            true
+        }
+    }
+
+    pub fn peek(&self) {}
 }
