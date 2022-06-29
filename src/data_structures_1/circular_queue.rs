511940d30e976e065bd8ec0e3ea983166d12b564
raman1383 <raman83shariati@gmail.com>
2022-06-29 12:46:38 +0430

    r

--- src/data_structures_1/circular_queue.rs
+++ src/data_structures_1/circular_queue.rs
@@ -1,3 +1,21 @@
-pub struct CircularQueue {}
+pub struct CircularQueue {
+    items: Vec<isize>,
+    rear_ptr: isize,
+    front_ptr: isize,
+}
 
-impl CircularQueue {}
+impl CircularQueue {
+    pub fn new(capacity: usize) -> CircularQueue {
+        CircularQueue {
+            items: vec![0; capacity],
+            rear_ptr: -1,
+            front_ptr: -1,
+        }
+    }
+
+    pub fn dequeue(&mut self) {}
+
+    pub fn enqueue(&mut self, item_to_enqueue: isize) {}
+
+    pub fn print_out(&self) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::ExitCode;
+
 pub mod data_structures_1;
 pub mod data_structures_2;
 pub mod dynamic_programming;
@@ -8,4 +10,6 @@ pub mod sorting_and_searching_algs;
 pub mod tree_based_dsa_1;
 pub mod tree_based_dsa_2;
 
-fn main() {}
+fn main() -> ExitCode {
+    ExitCode::SUCCESS
+}
