03e698930f386a8b3379cfdbc867526918d117ed
raman1383 <raman83shariati@gmail.com>
2022-06-29 06:52:09 +0430

    r

--- src/data_structures_1/queue.rs
+++ src/data_structures_1/queue.rs
@@ -1,10 +1,30 @@
 /// A linear DS following FIFO
-pub struct Queue {}
+pub struct Queue {
+    rear_ptr: isize,
+    front_ptr: isize,
+    items: Vec<isize>,
+}
 
 impl Queue {
-    // pub fn new() -> Self {}
+    pub fn new() -> Self {
+        Queue {
+            rear_ptr: -1,
+            front_ptr: -1,
+            items: Vec::with_capacity(10),
+        }
+    }
 
-    pub fn enqueue() {}
+    pub fn enqueue(&mut self) {}
 
     pub fn dequeue() {}
+
+    pub fn peek() {}
+
+    pub fn is_empty(&self) -> bool {
+        if self.front_ptr == -1 && self.rear_ptr == -1 {
+            true
+        } else {
+            false
+        }
+    }
 }
