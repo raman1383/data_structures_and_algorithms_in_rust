f45dbbfef8e908b446ec2fc66aebf6a5e12d8fe7
raman1383 <raman83shariati@gmail.com>
2022-06-29 07:39:09 +0430

    r

--- src/data_structures_1/queue.rs
+++ src/data_structures_1/queue.rs
@@ -1,30 +1,55 @@
+use std::borrow::BorrowMut;
+
 /// A linear DS following FIFO
-pub struct Queue {
-    rear_ptr: isize,
-    front_ptr: isize,
-    items: Vec<isize>,
+struct QueueNode<T> {
+    value: T,
+    next: Option<Box<QueueNode<T>>>,
 }
 
-impl Queue {
-    pub fn new() -> Self {
-        Queue {
-            rear_ptr: -1,
-            front_ptr: -1,
-            items: Vec::with_capacity(10),
+impl<T> QueueNode<T> {
+    fn new(value: T) -> Self {
+        QueueNode { value, next: None }
+    }
+}
+
+pub struct Queue<T> {
+    end: Option<QueueNode<T>>,
+}
+
+impl<T> Queue<T> {
+    pub fn remove(&mut self) -> Option<T> {
+        if !self.is_empty() {
+            let end = std::mem::take(&mut self.end).unwrap();
+            if let Some(next) = end.next {
+                self.end = Some(*next);
+            }
+            Some(end.value)
+        } else {
+            None
         }
     }
 
-    pub fn enqueue(&mut self) {}
-
-    pub fn dequeue() {}
-
-    pub fn peek() {}
+    pub fn add(&mut self, value: T) {
+        let new_node = QueueNode::new(value);
+        if let Some(end) = &mut self.end {
+            let mut start = end;
+            loop {
+                if let Some(_) = &start.next {
+                    start = (start.next.as_mut().unwrap()).borrow_mut();
+                } else {
+                    break;
+                }
+            }
+            start.next = Some(Box::new(new_node));
+        } else {
+            self.end = Some(new_node);
+        }
+    }
 
     pub fn is_empty(&self) -> bool {
-        if self.front_ptr == -1 && self.rear_ptr == -1 {
-            true
-        } else {
-            false
+        match self.end {
+            None => true,
+            _ => false,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ pub mod sorting_and_searching_algs;
 pub mod tree_based_dsa_1;
 pub mod tree_based_dsa_2;
 
-fn main() {}
+fn main() {
+    // let mut queue_1 = data_structures_1::queue::Queue::new();
+    // queue_1.enqueue(11);
+    // queue_1.enqueue(12);
+    // queue_1.enqueue(13);
+    // queue_1.print_out();
+
+    // queue_1.print_out();
+    // queue_1.enqueue(99);
+
+    // queue_1.print_out();
+}
