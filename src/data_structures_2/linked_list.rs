3df2d4636a4f5b999294eb678e54558976a6533d
raman1383 <raman83shariati@gmail.com>
2022-07-03 09:11:36 +0430

    r

--- src/data_structures_2/linked_list.rs
+++ src/data_structures_2/linked_list.rs
@@ -1,3 +1,50 @@
-pub struct LinkedList {}
+#![allow(non_camel_case_types)]
 
-impl LinkedList {}
+#[derive(Clone)]
+pub struct LinkedList {
+    value: u32,
+    next: Address,
+}
+
+impl LinkedList {
+    pub fn append(&mut self, elem: u32) {
+        match self.next {
+            Address::address(ref mut next_address) => {
+                next_address.append(elem);
+            }
+            Address::Nil => {
+                let node = LinkedList {
+                    value: elem,
+                    next: Address::Nil,
+                };
+                self.next = Address::address(Box::new(node))
+            }
+        }
+    }
+
+    pub fn delete(&mut self, elem: u32) {
+        match self.next {
+            Address::address(ref mut next_address) => {
+                if next_address.value == elem {
+                    println!("Deleting value {}", next_address.value);
+                    self.next = next_address.next.clone();
+                } else {
+                    next_address.delete(elem);
+                }
+            }
+            Address::Nil => {
+                if self.value == elem {
+                    self.value = 0;
+                } else {
+                    println!("Element {} does not exist in the linked list", elem);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+enum Address {
+    address(Box<LinkedList>),
+    Nil,
+}
